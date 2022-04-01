5676c73244b00c4f7469e1e34846bb63d69a46b8
oittaa <8972248+oittaa@users.noreply.github.com>
2022-04-01 05:31:53 +0300

    IV from salt and password -> smaller ciphertext (#5)

--- src/AESGCM.php
+++ src/AESGCM.php
@@ -6,23 +6,30 @@ namespace AESGCM;
 
 final class AESGCM
 {
+    private static function keyAndIV(string $password, string $salt): array
+    {
+        $key = hash_pbkdf2('sha512', $password, $salt, 100_000, 44, true);
+        $iv = substr($key, 32, 12);
+        $key = substr($key, 0, 32);
+        return [$key, $iv];
+    }
+
     public static function decrypt(string $data, string $password, bool $raw = false): string|false
     {
         if (!$raw) {
             $data = base64_decode($data, true);
         }
-        if ($data === false || strlen($data) < 60) {
+        if ($data === false || strlen($data) < 32) {
             return false;
         }
-        $salt = substr($data, 0, 32);
-        $iv = substr($data, 32, 12);
-        $tag = substr($data, 44, 16);
-        $data = substr($data, 60);
-        $secret = hash_pbkdf2('sha256', $password, $salt, 100000, 32, true);
+        $salt = substr($data, 0, 16);
+        $tag = substr($data, 16, 16);
+        $data = substr($data, 32);
+        [$key, $iv] = self::keyAndIV($password, $salt);
         return openssl_decrypt(
             $data,
             'aes-256-gcm',
-            $secret,
+            $key,
             OPENSSL_RAW_DATA,
             $iv,
             $tag,
@@ -32,20 +39,19 @@ final class AESGCM
 
     public static function encrypt(string $data, string $password, bool $raw = false): string
     {
-        $salt = random_bytes(32);
-        $iv = random_bytes(12);
-        $secret = hash_pbkdf2('sha256', $password, $salt, 100000, 32, true);
+        $salt = random_bytes(16);
+        [$key, $iv] = self::keyAndIV($password, $salt);
         $encrypted = openssl_encrypt(
             $data,
             'aes-256-gcm',
-            $secret,
+            $key,
             OPENSSL_RAW_DATA,
             $iv,
             $tag,
             'aes-256-gcm-php',
             16
         );
-        $result = $salt . $iv . $tag . $encrypted;
+        $result = $salt . $tag . $encrypted;
         if (!$raw) {
             $result = base64_encode($result);
         }
